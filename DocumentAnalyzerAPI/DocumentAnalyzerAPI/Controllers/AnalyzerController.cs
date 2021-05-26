1307a756d95d66219117d667956e7a6b4e41586e
Arturo Cordoba <arturocv16@gmail.com>
2021-05-25 18:19:56 -0600

    Get request to obtain Azure key

--- DocumentAnalyzerAPI/DocumentAnalyzerAPI/Controllers/AnalyzerController.cs
+++ DocumentAnalyzerAPI/DocumentAnalyzerAPI/Controllers/AnalyzerController.cs
@@ -66,7 +66,11 @@ namespace DocumentAnalyzerAPI.Controllers
             }
         }*/
 
-
+        [HttpGet, Route("/object")]
+        public IActionResult GetAzure()
+        {
+            return Ok("https://soafiles.blob.core.windows.net/files?sp=racwdl&st=2021-04-30T21:44:27Z&se=2021-06-16T05:44:27Z&sv=2020-02-10&sr=c&sig=kpTVVN2JZryg0FrW4fxWmFURSo0yUbkVtZZrS4e2dws%3D");
+        }
 
         [HttpGet, Route("/documents")]
         public IActionResult GetUserDocuments()
