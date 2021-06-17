2b1b8654cace893a8ade4b9939992ade3e3b95d9
Arturo Cordoba <arturocv16@gmail.com>
2021-06-17 17:01:16 -0600

    Kubernetes websocket config

--- Kubernetes/update.sh
+++ Kubernetes/update.sh
@@ -2,3 +2,4 @@ cd keycloak-db && ./update.sh && cd ..
 cd mongodb     && ./update.sh && cd ..
 cd postgres    && ./update.sh && cd ..
 cd keycloak    && ./update.sh && cd ..
+cd websocket   && ./update.sh && cd ..
