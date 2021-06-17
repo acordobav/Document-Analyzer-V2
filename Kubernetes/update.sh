51db08a205663faca5f09aad0085785ecef71d07
Arturo Cordoba <arturocv16@gmail.com>
2021-06-17 17:47:20 -0600

    EmployeeFinder Kubernetes config

--- Kubernetes/update.sh
+++ Kubernetes/update.sh
@@ -1,5 +1,8 @@
-cd keycloak-db && ./update.sh && cd ..
-cd mongodb     && ./update.sh && cd ..
-cd postgres    && ./update.sh && cd ..
-cd keycloak    && ./update.sh && cd ..
-cd websocket   && ./update.sh && cd ..
+cd keycloak-db         && ./update.sh && cd ..
+cd mongodb             && ./update.sh && cd ..
+cd postgres            && ./update.sh && cd ..
+cd keycloak            && ./update.sh && cd ..
+cd websocket           && ./update.sh && cd ..
+cd rabbitmq            && ./update.sh && cd ..
+cd documentanalyzerapi && ./update.sh && cd ..
+cd websocket           && ./update.sh && cd ..
