b73aad26dcd8cc775c1e83142d4c4e2b0793cf03
Arturo Cordoba <arturocv16@gmail.com>
2021-06-17 16:50:04 -0600

    Update postgres database kubernetes config

--- Kubernetes/postgres/delete.sh
+++ Kubernetes/postgres/delete.sh
@@ -3,4 +3,4 @@ kubectl delete service docanalyzer-postgres
 kubectl delete configmap docanalyzer-postgres-config
 kubectl delete pvc docanalyzer-postgres-v-claim
 kubectl delete pv docanalyzer-postgres-volume
-
+kubectl delete secrets docanalyzer-postgres-secret
